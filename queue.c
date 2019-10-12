43bf9523d45606b2f221d6f4c43da170aef753b0
FERNANDO DO NASCIMENTO VIEIRA <35479927+FernandoNv@users.noreply.github.com>
2019-10-12 18:27:34 -0300

    Add files via upload

---
+++ queue.c
@@ -0,0 +1,78 @@
+#include"queue.h"
+
+Queue* qCreate(){
+    return NULL;
+}
+
+Queue* qPush(Queue *q, Type info){
+    if(!q){
+        Queue *aux = malloc(sizeof(Queue));
+        aux->info = info;
+        aux->next = NULL;
+
+        return aux;
+    }
+    Queue *aux1 = q;
+    while(aux1->next){
+        aux1 = aux1->next;
+    }
+    Queue *aux = malloc(sizeof(Queue));
+    aux->info = info;
+    aux->next = NULL;
+
+    aux1->next = aux;
+
+    return q;
+}
+
+int qIsEmpty(Queue *q){
+    if(!q) return 1;
+    return 0;
+}
+Queue* qRemove(Queue *q, Type info){
+    if(!q) return q;
+    if(q->info == info){
+        Queue *aux = q;
+        aux = aux->next;
+        free(q);
+
+        return aux;
+    }
+    Queue *q1 = q;
+    Queue *q2 = q->next;
+    if(q2->info == info){
+        q1->next = q2->next;
+        free(q2);
+
+        return q1;
+    }
+
+    while(q2){
+        if(q2->info == info){
+            q1->next = q2->next;
+            free(q2);
+
+            return q;
+        }
+        q2 = q2->next;
+        q1 = q1->next;
+    }
+
+    return NULL;
+}
+Queue* qPop(Queue *q){
+    return qRemove(q, q->info);
+}
+Type qPeek(Queue *q){
+    return q->info;
+}
+Queue* qDestroy(Queue *q){
+    if(!q) return q;
+    q = qRemove(q, q->info);
+    return qDestroy(q);
+}
+void qPrint(Queue *q){
+    if(!q) return;
+    printf("%d ", q->info);
+    qPrint(q->next);
+}
---
+++ queue.h
@@ -0,0 +1,21 @@
+#ifndef QUEUE_H_INCLUDED
+#define QUEUE_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Type;
+typedef struct Queue{
+    Type info;
+    struct Queue *next;
+} Queue;
+
+Queue* qCreate();
+Queue* qPush(Queue *q, Type info);
+int qIsEmpty(Queue *q);
+Queue* qRemove(Queue *q, Type info);
+Queue* qPop(Queue *q);
+Type qPeek(Queue *q);
+Queue* qDestroy(Queue *q);
+void qPrint(Queue *q);
+#endif // QUEUE_H_INCLUDED
